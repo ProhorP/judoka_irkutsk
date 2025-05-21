c409db91ebef81ed50c8a926e7883a9fd30caad8
Petr Usoltsev <usoltsevpv@altlinux.org>
2025-05-21 17:29:27 +0800

    Telegram Боты на Aiogram 3.x: Текстовая клавиатура и Командное меню

--- keyboards/all_kb.py
+++ keyboards/all_kb.py
@@ -1,5 +1,14 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, KeyboardButtonPollType
 from create_bot import admins
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types import BotCommand, BotCommandScopeDefault
+from create_bot import bot
+
+async def set_commands():
+    commands = [BotCommand(command='start', description='Старт'),
+                BotCommand(command='start_2', description='Старт 2'),
+                BotCommand(command='start_3', description='Старт 3')]
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
 
 def main_kb(user_telegram_id: int):
     kb_list = [
@@ -7,6 +16,31 @@ def main_kb(user_telegram_id: int):
         [KeyboardButton(text="📝 Заполнить анкету"), KeyboardButton(text="📚 Каталог")]
     ]
     if user_telegram_id in admins:
-        kb_list.append([KeyboardButton(text="⚙️ Админ панель")])
-    keyboard = ReplyKeyboardMarkup(keyboard=kb_list, resize_keyboard=True, one_time_keyboard=True)
+        kb_list.append([KeyboardButton(text="⚙️ Админ панель111")])
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=kb_list,
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        input_field_placeholder="Воспользуйтесь меню:"
+    )
     return keyboard
+
+def create_spec_kb():
+    kb_list = [
+        [KeyboardButton(text="Отправить гео", request_location=True)],
+        [KeyboardButton(text="Поделиться номером", request_contact=True)],
+        [KeyboardButton(text="Отправить викторину/опрос", request_poll=KeyboardButtonPollType())]
+    ]
+    keyboard = ReplyKeyboardMarkup(keyboard=kb_list,
+                                   resize_keyboard=True,
+                                   one_time_keyboard=True,
+                                   input_field_placeholder="Воспользуйтесь специальной клавиатурой:")
+    return keyboard
+
+def create_rat():
+    builder = ReplyKeyboardBuilder()
+    for item in [str(i) for i in range(1, 11)]:
+        builder.button(text=item)
+    builder.button(text='Назад')
+    builder.adjust(4, 4, 2, 1)
+    return builder.as_markup(resize_keyboard=True)
