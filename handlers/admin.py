0f66e6874c102e6c8a233fb7aa6b2502c391b150
prohorp <usoltsev05@mail.ru>
2025-05-25 01:12:31 +0800

    admin role panel

--- handlers/admin.py
+++ handlers/admin.py
@@ -3,9 +3,10 @@ from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
+from create_bot import admins
 
 admin_router = Router()
 
-@admin_router.message(F.text.contains('Админ панель'))
+@admin_router.message((F.text.contains('Админ панель')) & (F.from_user.id.in_(admins)))
 async def start_profile(message: Message, state: FSMContext):
     await message.answer('Admin panel activate')
